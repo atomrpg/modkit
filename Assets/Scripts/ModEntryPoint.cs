e573ff19937261675f2c31d60eb17a73a6420723
atomrpg <theatomgame@gmail.com>
2019-04-14 22:15:11 +0300

    add suport file system bundle

--- Assets/Scripts/ModEntryPoint.cs
+++ Assets/Scripts/ModEntryPoint.cs
@@ -6,12 +6,21 @@ using System.Reflection;
 
 public class ModEntryPoint : MonoBehaviour // ModEntryPoint - RESERVED LOOKUP NAME
 {
+    readonly bool FILE_SYSTEM_LOAD = true;
     void Start()
     {
         var assembly = GetType().Assembly;
         string modName = assembly.GetName().Name;
         string dir = System.IO.Path.GetDirectoryName(assembly.Location);
         Debug.Log("Mod Init: " + modName + "(" + dir + ")");
-        ResourceManager.AddBundle(modName, AssetBundle.LoadFromFile(dir + "/" + modName + "_resources"));
+
+        if (FILE_SYSTEM_LOAD)
+        {
+            ResourceManager.AddBundle(modName, new FileSystemBundle("Avatar_Res"));
+        }
+        else
+        {
+            ResourceManager.AddBundle(modName, AssetBundle.LoadFromFile(dir + "/" + modName + "_resources"));
+        }
     }
 }
