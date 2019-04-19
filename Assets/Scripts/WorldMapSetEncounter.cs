84f8bbcfc7b4b0dbac16746ee260af2b6f21d6d8
atomrpg <theatomgame@gmail.com>
2019-04-20 00:22:26 +0300

    add simple world map example

---
+++ Assets/Scripts/WorldMapSetEncounter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMapSetEncounter : MonoBehaviour
+{
+    public EncountersProto _encounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var wmv = GetComponentInChildren<WorldMapView>();
+
+        //TOOD: make public
+        var prop = wmv.GetType().GetField("_encounter", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        prop.SetValue(wmv, _encounter);
+    }
+}
