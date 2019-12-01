c737e0726a28fb15454e9ea87480c3b65d43715f
atomrpg <theatomgame@gmail.com>
2019-12-01 18:09:46 +0200

    Example for provide custom node in game

---
+++ Assets/Editor/NodeEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using BehaviorEditor;
+
+[NodeEditor(typeof(CharacterHeroNode))]
+[CategoryAttribute("Character")]
+public class CharacterHero : FindObject
+{
+    protected override void OnNodeGUI()
+    {
+        base.OnNodeGUI();
+        GUILayout.Label("<i>" + GetState().msg + "</i>");
+    }
+
+    private CharacterHeroNode GetState()
+    {
+        return State as CharacterHeroNode;
+    }
+
+    protected override void OnNodeInspectorGUI()
+    {
+        base.OnNodeInspectorGUI();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Data:");
+        GetState().msg = EditorGUILayout.TextField(GetState().msg);
+        EditorGUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Scripts/CharacterHeroNode.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterHeroNode : FindObjectNode
+{
+    public string msg = "I'M SUPER HERO!";
+
+    public override void Start()
+    {
+        var cc = GetObject().GetComponent<CharacterComponent>();
+        CharacterProtoStats stats = cc.Character.CharProto.Stats;
+        for (CharacterProtoStats.Primary it = 0; it <= CharacterStats.Primary.Luck; ++it)
+        {
+            stats.SetPrimaryValue(it, 10);
+        }
+
+        for (CharacterProtoStats.SkillValue it = 0; it != CharacterStats.SkillValue.Max; ++it)
+        {
+            stats.SetSkillBonus(it, 100);
+        }
+
+        Game.World.Msg(true, msg);
+    }
+
+
+    public override void Serialize(JSon.JNode node)
+    {
+        base.Serialize(node);
+
+        node["msg"] = msg;
+    }
+
+    public override void Deserialize(JSon.JNode node)
+    {
+        base.Deserialize(node);
+
+        msg = node["msg"].Value;
+    }
+}
