13680b7ef59941e5f3ac5d195570b44472c9a881
sxdemon88 <sxdemon1988@gmail.com>
2020-04-24 17:49:46 +0300

    video example

--- Assets/Scripts/ModEntryPoint.cs
+++ Assets/Scripts/ModEntryPoint.cs
@@ -6,7 +6,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 //[assembly: AssemblyTitle("My Mod")] // ENTER MOD TITLE
-
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class ModEntryPoint : MonoBehaviour // ModEntryPoint - RESERVED LOOKUP NAME
 {
@@ -24,6 +26,7 @@ public class ModEntryPoint : MonoBehaviour // ModEntryPoint - RESERVED LOOKUP NA
     void GameLoaded(GlobalEvents.GameStart evnt)
     {
         Localization.LoadStrings("mymod_strings_");
+        Localization.LoadTexts("mymod_text_");
         Game.World.console.DeveloperMode();
     }
 
@@ -34,6 +37,32 @@ public class ModEntryPoint : MonoBehaviour // ModEntryPoint - RESERVED LOOKUP NA
 
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.F2))
+        {
+            Game.World.NextLevel("MyMod", "EnterPoint", true, false);
+        }
     }
 }
+#if UNITY_EDITOR
+
+[InitializeOnLoad]
+internal class LocalizationPreviewInEditor
+{
+    static LocalizationPreviewInEditor()
+    {
+        EditorApplication.update += Init;
+    }
+
+
+    static void Init()
+    {
+        if (!EditorApplication.isCompiling && ResourceManager.bundles.Count > 0)
+        {
+            EditorApplication.update -= Init;
+            Localization.Setup("ru", false);
+            Localization.LoadStrings("mymod_strings_");
+            Localization.LoadTexts("mymod_text_");
+        }
+    }
+}
+#endif
