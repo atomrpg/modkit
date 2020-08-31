b4582253fd27baee2f288e4c22eba2c9463851c3
atomrpg <theatomgame@gmail.com>
2020-09-01 02:38:43 +0300

    up

--- Assets/Scripts/ModEntryPoint.cs
+++ Assets/Scripts/ModEntryPoint.cs
@@ -1,13 +1,56 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JSon;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using UnityEngine.Video;
 
-public class ModEntryPoint : MonoBehaviour // ModEntryPoint - RESERVED LOOKUP NAME
+[assembly: AssemblyTitle("German Translation")]
+
+
+public class ModEntryPoint : MonoBehaviour, ILocalizedText // ModEntryPoint - RESERVED LOOKUP NAME
 {
+
+
     void Start()
     {
         var assembly = GetType().Assembly;
         string modName = assembly.GetName().Name;
         string dir = System.IO.Path.GetDirectoryName(assembly.Location);
         Debug.Log("Mod Init: " + modName + "(" + dir + ")");
+
+        if (!SettingLanguage.AvaibleLanguage.Contains("de"))
+        {
+            SettingLanguage.AvaibleLanguage.Add("de");
+        }
+
+        ScriptsPatch();
         ResourceManager.StreamingAssetsPath = dir + "/" + modName + "_External";
+
+    }
+
+    void ScriptsPatch()
+    {
+        var assembly = GetType().Assembly;
+        string modName = assembly.GetName().Name;
+        string dir = System.IO.Path.GetDirectoryName(assembly.Location);
+        ResourceManager.AddBundle(modName, AssetBundle.LoadFromFile(dir + "/" + modName + "_resources"));
+   
+    }
+
+    void ILocalizedText.UpdateText()
+    {
+        PatchLangList();
+    }
+
+    void PatchLangList()
+    {
+        Localization.LoadStrings("patch_strings_");
+    }
+
+    void GameLoaded(GlobalEvents.GameStart evnt)
+    {
+        PatchLangList();
     }
 }
