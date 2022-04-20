43f08d89bbc21a76c0d5a11efa79cfb7930c0ff2
d_martynenko <d_martynenko@atomrpg.com>
2022-04-20 18:39:56 +0300

    Fix test mod

--- Assets/Scripts/ModEntryPoint.cs
+++ Assets/Scripts/ModEntryPoint.cs
@@ -98,53 +98,6 @@ public class ModEntryPoint : MonoBehaviour // ModEntryPoint - RESERVED LOOKUP NA
         Game.World.console.DeveloperMode();
     }
 
-#if SUPPORT_LEVEL_BUNDLE
-    List<AssetBundle> lastLevelBundle = new List<AssetBundle>();
-    void PrepareNextLevel(GlobalEvents.PrepareNextLevel evnt)
-    {
-        if (manifest != null)
-        {
-            if (lastLevelBundle.Count > 0)
-            {
-                Debug.Log("Unload last level bundle: " + evnt.levelName);
-
-                foreach (var bundle in lastLevelBundle)
-                {
-                    ResourceManager.RemoveBundle(bundle, true);
-                }
-            }
-
-
-            AssetBundle b;
-
-            foreach (var bundle in manifest.GetAllDependencies(modName + "_" + evnt.levelName))
-            {
-                if(bundle.Contains("_resources"))
-                {
-                    continue; // skip default resources pack
-                }
-
-                Debug.Log("Load shared level bundle: " + bundle);
-
-                b = AssetBundle.LoadFromFile(dir + "/" + bundle);
-                if (b != null)
-                {
-                    ResourceManager.AddBundle(bundle, b);
-                    lastLevelBundle.Add(b);
-                }
-            }
-
-            Debug.Log("Load level bundle: " + evnt.levelName);
-            b = AssetBundle.LoadFromFile(dir + "/" + modName + "_" + evnt.levelName);
-            if (b != null)
-            {
-                ResourceManager.AddBundle(modName, b);
-                lastLevelBundle.Add(b);
-            }
-        }
-    }
-#endif
-
     void LevelLoaded(GlobalEvents.LevelLoaded evnt)
     {
         Debug.Log(evnt.levelName);
