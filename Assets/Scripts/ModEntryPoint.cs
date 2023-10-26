f3deb89fd0f93fd402a16da1abed0550f23eb45e
d_martynenko <d_martynenko@atomrpg.com>
2023-10-27 02:07:05 +0300

    new lang mod

--- Assets/Scripts/ModEntryPoint.cs
+++ Assets/Scripts/ModEntryPoint.cs
@@ -1,110 +1,62 @@
-﻿//#define SUPPORT_LEVEL_BUNDLE // Managed via build mod tool.
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using JSon;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-//[assembly: AssemblyTitle("My Mod")] // ENTER MOD TITLE
+[assembly: AssemblyTitle("TR")] // ENTER MOD TITLE
 
 
-public class ModEntryPoint : MonoBehaviour // ModEntryPoint - RESERVED LOOKUP NAME
+public class ModEntryPoint : MonoBehaviour, ILocalizedText // ModEntryPoint - RESERVED LOOKUP NAME
 {
-    string modName;
-    string dir;
+    void ILocalizedText.UpdateText()
+    {
+        PatchLangList();
+    }
+
+    void PatchLangList()
+    {
+        Localization.LoadStrings("patch_strings_");
+    }
 
     void Start()
     {
         var assembly = GetType().Assembly;
-        modName = assembly.GetName().Name;
-        dir = System.IO.Path.GetDirectoryName(assembly.Location);
+        string modName = assembly.GetName().Name;
+        string dir = System.IO.Path.GetDirectoryName(assembly.Location);
         Debug.Log("Mod Init: " + modName + "(" + dir + ")");
+        ResourceManager.AddBundle(modName, AssetBundle.LoadFromFile(dir + "/" + modName + "_resources"));
 
         GlobalEvents.AddListener<GlobalEvents.GameStart>(GameLoaded);
-        GlobalEvents.AddListener<GlobalEvents.LevelLoaded>(LevelLoaded);
-		
-		LoadModBundle();
-    }
 
-    void LoadModBundle()
-    {
-#if UNITY_EDITOR
-        // skip bundle loading in PIE mode
-#else
-        ResourceManager.AddBundle(modName, AssetBundle.LoadFromFile(dir + "/" + modName + "_resources"));
-#if SUPPORT_LEVEL_BUNDLE
-        AssetBundle assetBundle = AssetBundle.LoadFromFile(dir + "/" + modName);
-        if (assetBundle != null)
         {
-            manifest = assetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+            SettingLanguage.AvaibleLanguage.Add("tr");
         }
-        GlobalEvents.AddListener<GlobalEvents.PrepareNextLevel>(PrepareNextLevel);
-#endif
-#endif
-    }
 
-#if SUPPORT_LEVEL_BUNDLE
-    AssetBundleManifest manifest;
-    List<AssetBundle> lastLevelBundle = new List<AssetBundle>();
-    void PrepareNextLevel(GlobalEvents.PrepareNextLevel evnt)
-    {
-        if (manifest != null)
+        /* out of date
         {
-            if (lastLevelBundle.Count > 0)
-            {
-                Debug.Log("Unload last level bundle: " + evnt.levelName);
-
-                foreach (var bundle in lastLevelBundle)
-                {
-                    ResourceManager.RemoveBundle(bundle, true);
-                }
-            }
-
-
-            AssetBundle b;
-
-            foreach (var bundle in manifest.GetAllDependencies(modName + "_" + evnt.levelName))
-            {
-                if(bundle.Contains("_resources"))
-                {
-                    continue; // skip default resources pack
-                }
-
-                Debug.Log("Load shared level bundle: " + bundle);
-
-                b = AssetBundle.LoadFromFile(dir + "/" + bundle);
-                if (b != null)
-                {
-                    ResourceManager.AddBundle(bundle, b);
-                    lastLevelBundle.Add(b);
-                }
-            }
-
-            Debug.Log("Load level bundle: " + evnt.levelName);
-            b = AssetBundle.LoadFromFile(dir + "/" + modName + "_" + evnt.levelName);
-            if (b != null)
-            {
-                ResourceManager.AddBundle(modName, b);
-                lastLevelBundle.Add(b);
-            }
+            var field = typeof(Localization).GetField("_cultureInfo", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            field.SetValue(null, new System.Globalization.CultureInfo("en-US"));
         }
+        */
     }
-#endif
 
     void GameLoaded(GlobalEvents.GameStart evnt)
     {
-        Localization.LoadStrings("mymod_strings_");
-        Game.World.console.DeveloperMode();
-    }
-
-    void LevelLoaded(GlobalEvents.LevelLoaded evnt)
-    {
-        Debug.Log(evnt.levelName);
-    }
-
-    void Update()
-    {
-        
+        PatchLangList();
     }
 }
+
+#if UNITY_EDITOR
+[UnityEditor.InitializeOnLoad]
+public class Editor_Patch
+{
+    static Editor_Patch()
+    {
+        SettingLanguage.AvaibleLanguage.Add("tr");
+
+        Debug.Log("Editor: patch");
+    }
+}
+#endif
