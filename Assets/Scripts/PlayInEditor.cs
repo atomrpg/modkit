df61bc21da50ebae9ea9cf07c31fa6169aeb9575
d_martynenko <d_martynenko@atomrpg.com>
2023-11-17 22:37:02 +0200

    issues #14 fixed

--- Assets/Scripts/PlayInEditor.cs
+++ Assets/Scripts/PlayInEditor.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 #if UNITY_EDITOR
 using UnityEditor;
+using UnityEditor.SceneManagement;
 #endif
 
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 
 [ExecuteInEditMode]
 [DefaultExecutionOrder(-999)]
